88ff5e185e66d67538527d18e0775dae8f5e2710
unknown <akshatsoni64@gmail.com>
2023-08-23 17:47:29 +1000

    Task completed

---
+++ Client.java
@@ -0,0 +1,51 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
+            Socket connection = new Socket("localhost", 6666);
+            System.out.println("Connected to Server...");
+
+            DataInputStream dis = new DataInputStream(connection.getInputStream());
+
+            DataOutputStream dout = new DataOutputStream(connection.getOutputStream());
+            while (connection.isConnected()) {
+                String serverMessage = (String) dis.readUTF();
+                switch (serverMessage) {
+                    case "Hello":
+                        System.out.println(serverMessage);
+                        dout.writeUTF("Hello");
+                        dout.flush();
+                        break;
+                    case "Send Username":
+                        System.out.println("Enter Username:");
+                        String username = scanner.nextLine();
+                        dout.writeUTF(username);
+                        dout.flush();
+                        break;
+                    case "Send Password":
+                        System.out.println("Enter Password:");
+                        String password = scanner.nextLine();
+                        dout.writeUTF(password);
+                        dout.flush();
+                        break;
+                    case "See ya!":
+                        dout.close();
+                        connection.close();
+                        break;
+                }
+            }
+            scanner.close();
+        } catch (SocketException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ Server.java
@@ -0,0 +1,75 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            ServerSocket server = new ServerSocket(6666);
+            Socket connection = server.accept();
+
+            DataOutputStream dout = new DataOutputStream(connection.getOutputStream());
+            DataInputStream dis = new DataInputStream(connection.getInputStream());
+
+            String prompt = "\nSelect from available choices:\n";
+            prompt += "1: Greet Client\n";
+            prompt += "2: Authenticate\n";
+            prompt += "3: Terminate Connection";
+
+            System.out.println(prompt);
+            int input = scanner.nextInt();
+            while (connection.isConnected()) {
+                switch (input) {
+                    case 1:
+                        dout.writeUTF("Hello");
+                        dout.flush();
+                        break;
+                    case 2:
+                        dout.writeUTF("Send Username");
+                        dout.flush();
+
+                        String username = dis.readUTF();
+                        // System.out.print("Username: ");
+                        // System.out.println(username);
+
+                        dout.writeUTF("Send Password");
+                        dout.flush();
+
+                        String password = dis.readUTF();
+                        // System.out.print("Password: ");
+                        // System.out.println(password);
+
+                        Boolean authed = (username.equals("username") && password.equals("password"));
+                        String response = authed ? "Authentication Successful" : "Authentication Failed";
+                        System.out.println(response);
+
+                        break;
+                    case 3:
+                        dout.writeUTF("See ya!");
+                        dout.flush();
+
+                        connection.close();
+                        connection.shutdownInput();
+                        server.close();
+                        break;
+                    default:
+                        break;
+                }
+                System.out.println(prompt);
+                input = scanner.nextInt();
+                // String clientMessage = dis.readUTF();
+                // System.out.println("message= " + clientMessage);
+            }
+            scanner.close();
+        } catch (SocketException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+}
